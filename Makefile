86d7a447c968d044f6a1877cefa47b734d732bfb
Rob Conery <rob@conery.io>
2019-04-30 16:41:57 -1000

    Added db

--- Makefile
+++ Makefile
@@ -1,15 +1,11 @@
-APPNAME=velzyapp
-RG=velzy
-IMG=robconery/velzy
-
-build:
-	docker build -t $(IMG) .
-
-run:
-	docker run --rm -p 3000:3000 $(IMG)
+APPNAME=velzyappfree
+RG=velzyfree
 
 web:
-	DEBUG=005:* npm start
+	DEBUG=007:* npm start
+
+sense:
+	open https://$(APPNAME).scm.azurewebsites.net
 
 logs:
 	az webapp log tail -n $(APPNAME) -g $(RG) --verbose
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Datastore = require('nedb');
+const path = require("path");
+
+const dataFile = path.resolve(__dirname, "../data", "names.json");
+
+db = new Datastore({ filename: dataFile, autoload: true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  db.find({}, function(err,docs){
+    res.render('index', { names: docs});
+  })
+  
+});
+
+router.post("/", function(req, res, next){
+  const name = req.body.name;
+  console.log("Saving to..", dataFile);
+  db.insert({name: name}, function(err,newDoc){
+
+    res.redirect("/");
+  });
 });
 
 module.exports = router;
